358b8c5fe92812a83cbb8c2af18c75e57fc85fb3
Saif Khan <saifkhichi96@gmail.com>
2026-06-06 10:09:08 +0200

    feat(backends): remove unsupported OpenCV backend

--- src/spinepose/tools/base_tool.py
+++ src/spinepose/tools/base_tool.py
@@ -3,39 +3,22 @@ import os
 from abc import ABCMeta, abstractmethod
 from typing import Any
 
-import cv2
 import numpy as np
+import onnxruntime as ort
 
 from .utils.file import download_checkpoint
 
 
 def check_mps_support():
-    try:
-        import onnxruntime
-
-        providers = onnxruntime.get_available_providers()
-        return (
-            "MPSExecutionProvider" in providers
-            or "CoreMLExecutionProvider" in providers
-        )
-    except ImportError:
-        return False
+    providers = ort.get_available_providers()
+    return "CoreMLExecutionProvider" in providers
 
 
 RTMLIB_SETTINGS = {
-    "opencv": {
-        "cpu": (cv2.dnn.DNN_BACKEND_OPENCV, cv2.dnn.DNN_TARGET_CPU),
-        # You need to manually build OpenCV through cmake
-        "cuda": (cv2.dnn.DNN_BACKEND_CUDA, cv2.dnn.DNN_TARGET_CUDA),
-    },
-    "onnxruntime": {
-        "cpu": "CPUExecutionProvider",
-        "cuda": "CUDAExecutionProvider",
-        "rocm": "ROCMExecutionProvider",
-        "mps": "CoreMLExecutionProvider"
-        if check_mps_support()
-        else "CPUExecutionProvider",
-    },
+    "cpu": "CPUExecutionProvider",
+    "cuda": "CUDAExecutionProvider",
+    "rocm": "ROCMExecutionProvider",
+    "mps": "CoreMLExecutionProvider" if check_mps_support() else "CPUExecutionProvider",
 }
 
 
@@ -46,40 +29,16 @@ class BaseTool(metaclass=ABCMeta):
         model_input_size: tuple = None,
         mean: tuple = None,
         std: tuple = None,
-        backend: str = "opencv",
+        backend: str = "onnxruntime",
         device: str = "cpu",
     ):
         if not os.path.exists(onnx_model):
             onnx_model = download_checkpoint(onnx_model)
 
-        if backend == "opencv":
-            try:
-                providers = RTMLIB_SETTINGS[backend][device]
-
-                session = cv2.dnn.readNetFromONNX(onnx_model)
-                session.setPreferableBackend(providers[0])
-                session.setPreferableTarget(providers[1])
-                self.session = session
-            except Exception:
-                raise RuntimeError(
-                    "This model is not supported by OpenCV"
-                    " backend, please use `pip install"
-                    " onnxruntime` or `pip install"
-                    " onnxruntime-gpu` to install onnxruntime"
-                    " backend. Then specify `backend=onnxruntime`."
-                )  # noqa
-
-        elif backend == "onnxruntime":
-            import onnxruntime as ort
-
-            providers = RTMLIB_SETTINGS[backend][device]
-
-            self.session = ort.InferenceSession(
-                path_or_bytes=onnx_model, providers=[providers]
-            )
-
-        else:
-            raise NotImplementedError
+        providers = RTMLIB_SETTINGS[device]
+        self.session = ort.InferenceSession(
+            path_or_bytes=onnx_model, providers=[providers]
+        )
 
         logging.info(f"load {onnx_model} with {backend} backend")
 
@@ -110,16 +69,11 @@ class BaseTool(metaclass=ABCMeta):
         input = img[None, :, :, :]
 
         # run model
-        if self.backend == "opencv":
-            outNames = self.session.getUnconnectedOutLayersNames()
-            self.session.setInput(input)
-            outputs = self.session.forward(outNames)
-        elif self.backend == "onnxruntime":
-            sess_input = {self.session.get_inputs()[0].name: input}
-            sess_output = []
-            for out in self.session.get_outputs():
-                sess_output.append(out.name)
+        sess_input = {self.session.get_inputs()[0].name: input}
+        sess_output = []
+        for out in self.session.get_outputs():
+            sess_output.append(out.name)
 
-            outputs = self.session.run(sess_output, sess_input)
+        outputs = self.session.run(sess_output, sess_input)
 
         return outputs
